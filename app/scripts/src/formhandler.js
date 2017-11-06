042bfd23d129e8c9b005c164deec586fe77e9e4a
Elizabeth Nguyen <syaora@csu.fullerton.edu>
2017-11-06 14:05:47 -0800

    version 1.0

---
+++ app/scripts/src/formhandler.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+
+class FormHandler {
+  constructor (selector) {
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
+
+    this.$formElement = $(selector);
+    if (this.$formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }}
+
+  addSubmitHandler(fn) {
+    console.log('Setting submit handler for form');
+    this.$formElement.on('submit', (event) => {
+      event.preventDefault();
+
+      let data = {};
+      $(this).serializeArray().forEach((item) => {
+        data[item.name] = item.value;
+        console.log(item.name + ' is ' + item.value);
+      });
+      console.log(data);
+      fn(data);
+    });
+  }
+}
+export default FormHandler;
